96c369b36541e98b93971f6ab4997ca590f2cb8f
johnlok <john.lok.djinn@gmail.com>
2014-12-03 17:22:16 +0800

    tried to improve acceleration but doing so severely slows down gameplay.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,16 @@
-var ax = 0, ay = 0;
+var ax = 0, ay = 0,
+vx = 0, vy = 0, windowx = window.innerWidth,
+windowy = window.innerHeight, centerx = windowx / 2,
+centery = windowy / 2;
 
-if (window.DeviceMotionEvent != undefined) {
+setInterval( function() {
 window.ondevicemotion = function(e) {
-    ax = event.accelerationIncludingGravity.x * 100; //acceleration along x axis
-    ay = event.accelerationIncludingGravity.y * -100; //acceleration along y axis
+  ax = event.accelerationIncludingGravity.x * 10; //acceleration along x axis
+  ay = event.accelerationIncludingGravity.y * -10; //acceleration along y axis
+  vx = vx + ax;
+  vy = vy + ay;
   };
-}
+}, 15);
 
 var main = {
   preload: function() {
@@ -20,25 +25,28 @@ var main = {
 
   create: function() {
   game.physics.startSystem(Phaser.Physics.ARCADE);
-  ball = game.add.sprite(250, 250, 'ball');
+  ball = game.add.sprite(centerx, centery, 'ball');
   game.physics.enable(ball, Phaser.Physics.ARCADE);
   ball.body.collideWorldBounds = true;
   ball.body.bounce.set(1);
-  ball.body.velocity.setTo(0,0);
-
-
 
   },
 
   update: function() {
     // This function is called 60 times per second
     // It contains the game's logic
-  ball.body.velocity.setTo(ax,ay);
+    ball.body.velocity.setTo(vx,vy)
+  },
+
+  render: function() {
+     //debug helper
+    game.debug.spriteInfo(ball,32,32);
+
   },
 
 };
 
 // Initialize Phaser, and start our 'main' state
-var game = new Phaser.Game(500, 500, Phaser.AUTO, 'ballerDiv');
+var game = new Phaser.Game(windowx, windowy, Phaser.AUTO, 'ballerDiv');
 game.state.add('main', main);
 game.state.start('main');
