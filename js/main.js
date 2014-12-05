bfc59cec91b7582142522adcd38921e35d0fbc1f
johnlok <john.lok.djinn@gmail.com>
2014-12-05 15:12:52 +0800

    random ball size generator works

--- js/main.js
+++ js/main.js
@@ -15,22 +15,15 @@ window.ondevicemotion = function(e) {
   lastTime = now;
 };
 
-var enemies, enemy, playerBall, pBallSize;
+var enemies, enemy, playerBall, smallerEnemy, largerEnemy;
 
-function init() {
-  //code to be added here.
-}
-
-function calcPlayerSize(playerBall) {
-  pBallSize = playerBall.scale.x; // get the size of the ball. To be used in future functions.
-  return pBallSize;
-}
 
 var main = {
   preload: function() {
     // This function will be executed at the beginning
     // That's where we load the game's assets
     game.load.image('ball', 'assets/ball.png', 400, 400);
+    game.load.image('player', 'assets/player.png', 400, 400);
     game.stage.backgroundColor = '#FFFFFF';
 
   },
@@ -41,31 +34,50 @@ var main = {
     game.physics.startSystem(Phaser.Physics.ARCADE);
 
     //this is the player ball
-    playerBall = game.add.sprite(centerx, centery, 'ball');
+    playerBall = game.add.sprite(centerx, centery, 'player');
     game.physics.enable(playerBall, Phaser.Physics.ARCADE);
     playerBall.body.collideWorldBounds = true;
     playerBall.body.checkCollision = true;
     playerBall.body.bounce.set(0.9);
-    playerBall.scale.setTo(0.05,0.05);
+    playerBall.scale.setTo(0.1,0.1);
     playerBall.anchor.setTo(0.5, 0.5);
 
 
-    // create a bunch of randomly moving balls which bounce
+    // initial game state
 
     enemies = game.add.group();
     enemies.enableBody = true;
     enemies.physicsBodyType = Phaser.Physics.ARCADE;
 
-    for (var x = 0; x < 20; x++) {
+    for (var x = 1; x < 20; x++) {
       enemy = enemies.create(game.world.randomX, game.world.randomY, 'ball');
       enemy.body.bounce.set(1.01);
       var randv = game.rnd.realInRange(-500, 500);
       var randv2 = game.rnd.realInRange(-500, 500);
       enemy.body.velocity.setTo(randv, randv2);
       // var randsc = game.rnd.realInRange(0.01, 0.5);
-      enemy.scale.setTo(0.02,0.02); // make random sizes here
+      // enemy.scale.setTo(0.02, 0.02);
+
+      if (x <= 10) {
+        smallerEnemy = game.rnd.realInRange(0.001, playerBall.scale.x);
+        enemy.scale.setTo(smallerEnemy, smallerEnemy); // this makes 10 enemies smaller than the current playerBall.scale.x
+      }
+
+      else if (x > 10 && x <= 14) {
+        enemy.scale.setTo(playerBall.scale.x, playerBall.scale.x); // this makes 3 balls with the same size as playerBall.scale.x here note: can write a function to return two values or arguments?
+      }
+
+      else {
+        largerEnemy = game.rnd.realInRange(playerBall.scale.x, 0.3);
+        enemy.scale.setTo(largerEnemy, largerEnemy);
+      }
+
       enemy.body.collideWorldBounds = true;
-    }
+
+
+}
+
+
 
 
   },
@@ -81,8 +93,12 @@ var main = {
 };
 
 function eatBall (_playerBall, _enemy) {
-  _enemy.kill();
-  game.add.tween(playerBall.scale).to( { x: 0.2, y: 0.2 }, 50, Phaser.Easing.Linear.None, true, 0, 0, true);
+  if (_enemy.scale.x > _playerBall.scale.x) {
+    _enemy.kill();
+  }
+
+
+  // game.add.tween(playerBall.scale).to( { x: 0.2, y: 0.2 }, 50, Phaser.Easing.Linear.None, true, 0, 0, true);
 }
 
 // Initialize Phaser, and start our 'main' state
