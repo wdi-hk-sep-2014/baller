5886bfad051a56248e3096b0e7c625e06bc58a0f
johnlok <john.lok.djinn@gmail.com>
2014-12-09 16:11:31 +0800

    added welcome screen and "boot" state

--- js/app.js
+++ js/app.js
@@ -1,12 +1,8 @@
-var windowx = window.innerWidth,
-windowy = window.innerHeight, centerx = windowx / 2,
-centery = windowy / 2;
-
 function Game() {}
 
 Game.prototype = {
   start: function() {
-    var game = new Phaser.Game(windowx, windowy, Phaser.AUTO, 'ballerDiv');
+    game = new Phaser.Game(windowx, windowy, Phaser.AUTO, 'ballerDiv');
 
     game.state.add('boot', Boot);
     game.state.start('boot');
--- js/states/boot.js
+++ js/states/boot.js
@@ -1,15 +1,26 @@
-function Boot() {};
+var windowx = window.innerWidth,
+windowy = window.innerHeight,
+screenRatio = windowx / windowy,
+centerx = windowx / 2,
+centery = windowy / 2;
+logoWidth = windowx * 0.3;
+logoHeight = logoWidth * (9/16);
+
+function Boot() {}
 
 Boot.prototype = {
 
   preload: function() {
+    game.load.image('ballerlogo', '../assets/ballerlogo.png', 1200, 728);
     //load preloader assets
-    game.load.image('gameogre', 'assets/gameogre.png', 500, 256);
   },
 
   create: function() {
     //setup game environment
     //scale, input etc...
     // this.game.state.start('preload');
+    game.stage.backgroundColor = '#FFFFFF';
+    test = game.add.sprite(centerx, centery, 'ballerlogo');
+    test.anchor.setTo(0.5,0.5);
   }
 };
