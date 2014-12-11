fee7f15a3804ced4a3655fda0653598b0be595ea
johnlok <john.lok.djinn@gmail.com>
2014-12-11 15:26:04 +0800

    created \'accelerate away from player\' function but waas not good in terms of gameplay. looking for alternate solution

--- js/states/level_round.js
+++ js/states/level_round.js
@@ -19,7 +19,6 @@ function createSmallerEnemies() {
         game.physics.p2.enable(enemy, false);
         var randv = game.rnd.realInRange(-300, 300);
         var randv2 = game.rnd.realInRange(-300, 300);
-
         enemy.body.velocity.x = randv;
         enemy.body.velocity.y = randv2;
     }
@@ -34,12 +33,12 @@ function createLargerEnemies() {
     }
 }
 
-function moveTowardPlayer (enemy) {
+function moveLargerTowardPlayer (enemy) {
     accelerateToObject(enemy, playerBall, 200);
 }
 
-function moveAwayFromPlayer (enemy) {
-    accelerateFromObject(enemy, playerBall, 50);
+function moveSmallerTowardPlayer (enemy) {
+    accelerateToObject(enemy, playerBall, 50);
 }
 
 function accelerateToObject(obj1, obj2, speed) {
@@ -48,14 +47,6 @@ function accelerateToObject(obj1, obj2, speed) {
     obj1.body.rotation = angle + game.math.degToRad(90);  // correct angle of angry bullets (depends on the sprite used)
     obj1.body.force.x = Math.cos(angle) * speed;    // accelerateToObject
     obj1.body.force.y = Math.sin(angle) * speed;
-}
-
-function accelerateFromObject(obj1, obj2, speed) {
-    if (typeof speed === 'undefined') { speed = 60; }
-    var angle = Math.atan2(obj2.y - obj1.y, obj2.x - obj1.x);
-    obj1.body.rotation = angle + game.math.degToRad(90);  // correct angle of angry bullets (depends on the sprite used)
-    obj1.body.force.x = -Math.cos(angle) * speed;    // accelerateToObject
-    obj1.body.force.y = -Math.sin(angle) * speed;
 }
 
 LevelRoundState.prototype = {
@@ -67,12 +58,34 @@ LevelRoundState.prototype = {
 
 
   create: function() {
-    cursors = game.input.keyboard.createCursorKeys();
+
+
+
+
+
     game.physics.startSystem(Phaser.Physics.P2JS);
-    game.physics.p2.defaultRestitution = 0.8;
     smallerEnemies = game.add.group();
     largerEnemies = game.add.group();
+
     createPlayer();
+
+    var playerMaterial = game.physics.p2.createMaterial('playerMaterial', playerBall.body);
+
+    var worldMaterial = game.physics.p2.createMaterial('worldMaterial');
+
+    game.physics.p2.setWorldMaterial(worldMaterial, true, true, true, true);
+
+    var contactMaterial = game.physics.p2.createContactMaterial(playerMaterial, worldMaterial);
+
+    contactMaterial.friction = 0;     // Friction to use in the contact of these two materials.
+    contactMaterial.restitution = 1.2;  // Restitution (i.e. how bouncy it is!) to use in the contact of these two materials.
+    contactMaterial.stiffness = 1e7;    // Stiffness of the resulting ContactEquation that this ContactMaterial generate.
+    contactMaterial.relaxation = 3;     // Relaxation of the resulting ContactEquation that this ContactMaterial generate.
+    contactMaterial.frictionStiffness = 1e7;    // Stiffness of the resulting FrictionEquation that this ContactMaterial generate.
+    contactMaterial.frictionRelaxation = 3;     // Relaxation of the resulting FrictionEquation that this ContactMaterial generate.
+    contactMaterial.surfaceVelocity = 0;        // Will add surface velocity to this material. If bodyA rests on top if bodyB, and the surface velocity is positive, bodyA will slide to the right.
+
+
     createSmallerEnemies();
     createLargerEnemies();
 
@@ -84,8 +97,8 @@ LevelRoundState.prototype = {
   update: function() {
     // This function is called 60 times per second
     // It contains the game's logic
-    largerEnemies.forEachAlive(moveTowardPlayer, this); // make balls accelerate towards player
-    // smallerEnemies.forEachAlive
+    largerEnemies.forEachAlive(moveLargerTowardPlayer, this); // make balls accelerate towards player
+    smallerEnemies.forEachAlive(moveSmallerTowardPlayer, this);
 
     //keyboard movement
 
