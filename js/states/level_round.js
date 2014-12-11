3ad3f8afcbbe3bb74e106c67ad864752e5e37d5a
johnlok <john.lok.djinn@gmail.com>
2014-12-11 12:19:24 +0800

    created functions for creating larger and smaller enemies. need to set world boundary collision.

--- js/states/level_round.js
+++ js/states/level_round.js
@@ -1,6 +1,8 @@
 function LevelRoundState() {}
 
-var lives = 3, playerBall, enemy, enemies;
+var lives = 3, playerBall, enemy, smallerEnemies, largerEnemies;
+
+
 
 
 function createPlayer() {
@@ -9,18 +11,35 @@ function createPlayer() {
     game.physics.p2.enable(playerBall);
 }
 
-function createEnemies() {
-    enemies = game.add.group();
-    for (var x = 1; x < 20; x++) {
-        var enemy = enemies.create(game.world.randomX, game.world.randomY, 'ball');
+function createSmallerEnemies() {
+    for (var x = 1; x < 15; x++) {
+        var enemy = smallerEnemies.create(game.world.randomX, game.world.randomY, 'ball');
         var smallerEnemy = game.rnd.realInRange(0.001, playerBall.scale.x);
         enemy.scale.setTo(smallerEnemy, smallerEnemy); // this makes 14 enemies smaller than the current playerBall.scale.x
+        game.physics.p2.enable(enemy, false);
+        var randv = game.rnd.realInRange(-300, 300);
+        var randv2 = game.rnd.realInRange(-300, 300);
+
+        enemy.body.velocity.x = randv;
+        enemy.body.velocity.y = randv2;
+    }
+}
+
+function createLargerEnemies() {
+    for (var x = 1; x < 5; x++) {
+        var enemy = largerEnemies.create(game.world.randomX, game.world.randomY, 'ball');
+        var largerEnemy = game.rnd.realInRange(playerBall.scale.x, playerBall.scale.x * 1.5);
+        enemy.scale.setTo(largerEnemy, largerEnemy); // this makes 14 enemies smaller than the current playerBall.scale.x
         game.physics.p2.enable(enemy, false);
     }
 }
 
-function moveBalls (enemy) {
+function moveTowardPlayer (enemy) {
     accelerateToObject(enemy, playerBall, 200);
+}
+
+function moveAwayFromPlayer (enemy) {
+    accelerateFromObject(enemy, playerBall, 50);
 }
 
 function accelerateToObject(obj1, obj2, speed) {
@@ -29,6 +48,14 @@ function accelerateToObject(obj1, obj2, speed) {
     obj1.body.rotation = angle + game.math.degToRad(90);  // correct angle of angry bullets (depends on the sprite used)
     obj1.body.force.x = Math.cos(angle) * speed;    // accelerateToObject
     obj1.body.force.y = Math.sin(angle) * speed;
+}
+
+function accelerateFromObject(obj1, obj2, speed) {
+    if (typeof speed === 'undefined') { speed = 60; }
+    var angle = Math.atan2(obj2.y - obj1.y, obj2.x - obj1.x);
+    obj1.body.rotation = angle + game.math.degToRad(90);  // correct angle of angry bullets (depends on the sprite used)
+    obj1.body.force.x = -Math.cos(angle) * speed;    // accelerateToObject
+    obj1.body.force.y = -Math.sin(angle) * speed;
 }
 
 LevelRoundState.prototype = {
@@ -43,15 +70,25 @@ LevelRoundState.prototype = {
     cursors = game.input.keyboard.createCursorKeys();
     game.physics.startSystem(Phaser.Physics.P2JS);
     game.physics.p2.defaultRestitution = 0.8;
+    smallerEnemies = game.add.group();
+    largerEnemies = game.add.group();
     createPlayer();
-    createEnemies();
+    createSmallerEnemies();
+    createLargerEnemies();
+
+
+
 
   },
 
   update: function() {
     // This function is called 60 times per second
     // It contains the game's logic
-    enemies.forEachAlive(moveBalls, this); // make balls accelerate towards player
+    largerEnemies.forEachAlive(moveTowardPlayer, this); // make balls accelerate towards player
+    // smallerEnemies.forEachAlive
+
+    //keyboard movement
+
     if (cursors.left.isDown) {playerBall.body.rotateLeft(100);}   //playerBall movement
     else if (cursors.right.isDown){playerBall.body.rotateRight(100);}
     else {playerBall.body.setZeroRotation();}
