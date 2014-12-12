2253c8ad4ee3afb01c05c2c25ecc8245d1e3ba0d
johnlok <john.lok.djinn@gmail.com>
2014-12-12 12:22:35 +0800

    moved asset loading to preloader

--- js/states/preload.js
+++ js/states/preload.js
@@ -2,8 +2,28 @@ Preload.prototype = {
   preload: function() {
     // load all game assets
     // images, spritesheets, atlases, audio etc..
+
+    game.load.image('ballerlogo', 'assets/ballerlogo.png');
+    game.load.image('startbutton', 'assets/startbutton.png');
+    game.load.image('ball', 'assets/ball.png', 400, 400);
+    game.load.image('player', 'assets/player.png', 400, 400);
+    game.load.image('gameogre', 'assets/gameogre.png', 500, 256);
+    game.load.image('single', 'assets/singleplayer.png');
+    game.load.image('multi', 'assets/multiplayer.png');
+    game.load.image('options', 'assets/options.png');
+
   },
   create: function() {
-    this.game.state.start('main-intro');
+    // this.game.state.start('main-intro');
+
+    var logo = game.add.sprite(centerx, centery - 50, 'ballerlogo');
+    logo.anchor.setTo(0.5,0.5);
+    logo.scale.setTo(0.55,0.55);
+
+    var startgame = game.add.sprite(centerx, centery + 250, 'startbutton');
+    startgame.anchor.setTo(0.5,0.5);
+    startgame.scale.setTo(0.65,0.65);
+
+
   }
 };
