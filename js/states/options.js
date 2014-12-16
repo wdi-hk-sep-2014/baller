7451fb340fe8129e83c0fbb66831f947fed2cbf8
johnlok <john.lok.djinn@gmail.com>
2014-12-16 15:36:16 +0800

    sensitiviity can now be set by user. will add a back button next.

--- js/states/options.js
+++ js/states/options.js
@@ -1,9 +1,24 @@
 function optionsState() {}
 
-var inputSensitivity, optionsTitle;
+var optionsTitle, testBall, testCollisionGroup;
+var inputSensitivity = 400;
+
+function sensitivityTest() {
+  testBall = game.add.sprite(game.width - 50, game.height - 50, 'player');
+  testBall.scale.setTo(playerScale, playerScale);
+  game.physics.p2.enable(testBall);
+  testBall.body.setCollisionGroup(testCollisionGroup);
+}
 
 optionsState.prototype = {
   create: function() {
+    game.physics.startSystem(Phaser.Physics.P2JS);
+    game.physics.p2.setImpactEvents(true);
+    game.physics.p2.restitution = 0.95;
+    testCollisionGroup = game.physics.p2.createCollisionGroup();
+    game.physics.p2.updateBoundsCollisionGroup();
+
+    sensitivityTest();
 
     var optionsTitle = game.add.sprite(centerx, oneThirdHeight, 'sensitivity');
     optionsTitle.anchor.setTo(0.5,0.5);
@@ -13,8 +28,8 @@ optionsState.prototype = {
 
     var sensitivityOptions = [
       {name: 'sense_high', yOffset: 120},
-      {name: 'sense_medium', yOffset: 220},
-      {name: 'sense_low', yOffset: 320}
+      {name: 'sense_medium', yOffset: 250},
+      {name: 'sense_low', yOffset: 380}
     ];
 
     var sensitivitySprites = {};
@@ -30,6 +45,39 @@ optionsState.prototype = {
         sensitivitySprites[option.name] = sprite;
     });
 
+    // have some rotten code here. Will refactor.
+
+    function highSensitivity() {
+      inputSensitivity = 500;
+      game.add.tween(sensitivitySprites.sense_high.scale).to({x: 0.5, y: 0.5}, 500, Phaser.Easing.Quadratic.InOut, true);
+      game.add.tween(sensitivitySprites.sense_medium.scale).to({x: 0.3, y: 0.3}, 500, Phaser.Easing.Quadratic.InOut, true);
+      game.add.tween(sensitivitySprites.sense_low.scale).to({x: 0.3, y: 0.3}, 500, Phaser.Easing.Quadratic.InOut, true);
+      return;
+    }
+    function mediumSensitivity() {
+      inputSensitivity = 400;
+      game.add.tween(sensitivitySprites.sense_high.scale).to({x: 0.3, y: 0.3}, 500, Phaser.Easing.Quadratic.InOut, true);
+      game.add.tween(sensitivitySprites.sense_medium.scale).to({x: 0.5, y: 0.5}, 500, Phaser.Easing.Quadratic.InOut, true);
+      game.add.tween(sensitivitySprites.sense_low.scale).to({x: 0.3, y: 0.3}, 500, Phaser.Easing.Quadratic.InOut, true);
+      return;
+    }
+    function lowSensitivity() {
+      inputSensitivity = 300;
+      game.add.tween(sensitivitySprites.sense_high.scale).to({x: 0.3, y: 0.3}, 500, Phaser.Easing.Quadratic.InOut, true);
+      game.add.tween(sensitivitySprites.sense_medium.scale).to({x: 0.3, y: 0.3}, 500, Phaser.Easing.Quadratic.InOut, true);
+      game.add.tween(sensitivitySprites.sense_low.scale).to({x: 0.5, y: 0.5}, 500, Phaser.Easing.Quadratic.InOut, true);
+      return;
+    }
+
+    sensitivitySprites.sense_high.events.onInputDown.add(highSensitivity, this);
+    sensitivitySprites.sense_medium.events.onInputDown.add(mediumSensitivity, this);
+    sensitivitySprites.sense_low.events.onInputDown.add(lowSensitivity, this);
+
+  },
+
+  update: function() {
+    testBall.body.force.x = ax;
+    testBall.body.force.y = ay;
   }
 };
 
