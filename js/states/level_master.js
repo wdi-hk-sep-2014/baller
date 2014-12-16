0e2a2611b6861a136cc0181cbde5f6efa83bd50f
johnlok <john.lok.djinn@gmail.com>
2014-12-16 19:45:33 +0800

    added some new sprites, created a difficulty screen

--- js/states/level_master.js
+++ js/states/level_master.js
@@ -2,7 +2,15 @@ function LevelMasterState() {}
 
 LevelMasterState.prototype = {
   create: function() {
-    this.game.state.start('level_round');
+    var levelTitle = game.add.sprite(centerx, oneThirdHeight, 'set_level');
+    levelTitle.anchor.setTo(0.5,0.5);
+    levelTitle.scale.setTo(0.5,0.5);
+    levelTitle.alpha = 0;
+    var levelTitleAnimation = game.add.tween(levelTitle).to({alpha: 1}, 1000, Phaser.Easing.Quadratic.InOut, true);
+
+
+
+    // this.game.state.start('level_round');
   }
 };
 
